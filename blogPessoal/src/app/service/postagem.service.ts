60a5355cf6fee3be766b2f909c33ce48ee3a6932
mathews19 <48871936+mathews19@users.noreply.github.com>
2020-10-09 13:24:25 -0300

    arrumado2

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -16,18 +16,18 @@ export class PostagemService {
     return this.http.get('http://localhost:8080/postagens', this.token)
   }
   getByIdPostagem(id: number) {
-    return this.http.get(`http://localhost:9000/postagens/${id}`, this.token)
+    return this.http.get(`http://localhost:8080/postagens/${id}`, this.token)
   }
   postPostagem(postagem: Postagem) {
-    return this.http.post('http://localhost:9000/postagens', postagem, this.token)
+    return this.http.post('http://localhost:8080/postagens', postagem, this.token)
   }
   putPostagem(postagem: Postagem) {
-    return this.http.put('http://localhost:9000/postagens', postagem, this.token)
+    return this.http.put('http://localhost:8080/postagens', postagem, this.token)
   }
   deletePostagem(id: number) {
-    return this.http.delete(`http://localhost:9000/postagens/${id}`, this.token)
+    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
   }
   getByTituloPostagem(titulo: string) {
-    return this.http.get(`http://localhost:9000/postagens/titulo/${titulo}`, this.token)
+    return this.http.get(`http://localhost:8080/postagens/titulo/${titulo}`, this.token)
   }
 }

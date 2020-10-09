1de238ddbeffc0ed91919ebd7c96a3f3e69e2341
mathews19 <48871936+mathews19@users.noreply.github.com>
2020-10-09 12:30:29 -0300

    Reorganizando alguns componentes

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,6 @@
 import { UserLogin } from './../model/UserLogin';
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { User } from '../model/User';
 import { environment } from 'src/environments/environment.prod';
 
@@ -9,32 +9,36 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class AuthService {
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
+
   logar(userLogin: UserLogin) {
     return this.http.post('http://localhost:8080/usuarios/logar', userLogin)
   }
+
   cadastrar(user: User) {
     return this.http.post('http://localhost:8080/usuarios/cadastrar', user)
   }
 
   btnSair(){
     let ok = false
-    let token = localStorage.getItem('token')
+    let token = environment.token
 
-    if (token != null) {
+    if (token != '') {
       ok = true
     }
+
     return ok
   }
-    btnLogin() {
-      let ok = false
-      let token = localStorage.getItem('token')
-  
-      if (token == null) {
-        ok = true
-      }
-  
-      return ok
+
+  btnLogin() {
+    let ok = false
+    let token = environment.token
+
+    if (token == '') {
+      ok = true
     }
-  
+
+    return ok
+  }
+
 }
